533fcc8e89c730f659d4d74c7cbc8522bc696cfd
filipedasilvaxxx <vasconcelossilva791@gmail.com>
2019-03-25 21:30:48 -0300

    ionic-introducao

---
+++ src/pages/form-contate/form-contate.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { FormContatePage } from './form-contate';
+
+@NgModule({
+  declarations: [
+    FormContatePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(FormContatePage),
+  ],
+})
+export class FormContatePageModule {}
